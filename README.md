1d3758c550b5bea0d5e8b1e749f6ae75d4f7e843
ria <42520601+riazaia@users.noreply.github.com>
2025-05-17 20:39:00 -0300

    Fix bug where selectors weren't being properly created for multiclass elements

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # html2css
+
 A tool to extract HTML element tags, classes and IDs from pasted HTML and create a CSS skeleton for easy styling.
 
-## to do 
+## to do
+
 - option to make classes use descendant selectors
 - grab inline styles
--- script.js
+++ script.js
@@ -1,52 +1,87 @@
-// const HTML_REGEXP = /<(.*?)>/g;
-const TAG_REGEXP = /<(\w*?)[ >]/g;
-const CLASS_REGEXP = /class="(.*?)"/g;
-const ID_REGEXP = /id="(.*?)"/g;
+// The regular expressions used to find each type of future selector
+const REGEX = {
+  element: /<(\w*?)[ >]/g,
+  class: /class="(.*?)"/g,
+  id: /id="(.*?)"/g,
+};
 
-const EXCLUDED_TAGS = ["base", "head", "link", "meta", "style", "title", "canvas", "script", "noscript"];
+// A list of HTML elements that cannot be styled with CSS and therefore should not be included in the output
+const EXCLUDED_TAGS = [
+  "base",
+  "head",
+  "link",
+  "meta",
+  "style",
+  "title",
+  "canvas",
+  "script",
+  "noscript",
+];
 
-let btn = document.querySelector("#extractor");
-btn.addEventListener("click", update)
+// Select the button on the page and add a listener for a click event
+const btn = document.querySelector("#extractor");
+btn.addEventListener("click", updateOutput);
 
-function parse(rawHtml, options) {
-  const result = [];
+function updateOutput() {
+  const outputBox = document.querySelector("#output");
+  const inputBox = document.querySelector("#input");
+  const checkedOptions = document.querySelectorAll("input:checked");
+
+  let options = [];
+
+  checkedOptions.forEach((checkbox) => {
+    options.push(checkbox.getAttribute("id"));
+  });
+
+  if (options.length === 0) {
+    return alert("You must select an option!");
+  }
+
+  const foundSelectors = parseInput(inputBox.value, options);
+  outputBox.value = "";
+  outputBox.value = generateOutput(foundSelectors);
+}
+
+function parseInput(rawHtml, options) {
+  const allSelectors = [];
+
+  options.forEach((option) => {
+    allSelectors.push(...extractSelectors(rawHtml, option));
+  });
+  const uniqueSelectors = [...new Set(allSelectors)];
+  return uniqueSelectors;
+}
+
+function generateOutput(selectorsArr) {
   let stringOutput = "";
-  
-  function extract(regex, o) {
-    [...rawHtml.matchAll(regex)].forEach(([, match]) => {
-      if (o?.class === true) match = `.${match}`;
-      if (o?.id === true) match = `#${match}`;
-      if (!EXCLUDED_TAGS.includes(match) && !result.includes(match)) {
-        result.push(match);
-      }
-    });
-  }
-
-  if (options.includes("tags")) extract(TAG_REGEXP);
-  if (options.includes("classes")) extract(CLASS_REGEXP, { class: true });
-  if (options.includes("ids")) extract(ID_REGEXP, { id: true });
-
-  result.forEach((elem) => {
-    stringOutput += `${elem} {
-
-  }
+  selectorsArr.forEach((selector) => {
+    stringOutput += `${selector} {  }
 `;
   });
   return stringOutput;
 }
 
-function update() {
-  let outputBox = document.querySelector("#output");
-  let inputBox = document.querySelector("#input");
+function extractSelectors(input, type) {
+  const matchedSelectors = [];
+  const regex = REGEX[type];
+  const allMatches = [...input.matchAll(regex)];
 
-  let options = [];
-  Array.from(
-    document.querySelectorAll('input[type="checkbox"]:checked')
-  ).forEach((item) => {
-    options = [...options, item.getAttribute("id")];
+  allMatches.forEach(([, result]) => {
+    const match = result.split(" ");
+    if (match.length === 1) {
+      matchedSelectors.push(
+        ...match.map((keyword) => {
+          return type === "class"
+            ? `.${keyword}`
+            : type === "id"
+            ? `#${keyword}`
+            : !EXCLUDED_TAGS.includes(keyword)
+            ? `${keyword}`
+            : "";
+        })
+      );
+    } else {
+    }
   });
-
-  if (options.length === 0) return alert("You must select an option!");
-  outputBox.value = "";
-  outputBox.value = parse(inputBox.value, options);
+  return matchedSelectors;
 }
